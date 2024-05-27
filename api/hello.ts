df66005313f8f62f9671ef51a07044335a1fbc0e
MonishMuskar <166914969+MonishMuskar@users.noreply.github.com>
2024-05-27 16:26:03 +0530

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,19 +1,8 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-let requestCount = 0;
+import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  requestCount += 1;
-
-  if (requestCount % 20 === 0) {
-    return res.status(500).json({
-      error: 'Internal Server Error',
-      message: 'This is the 20th request, triggering an error as per design.',
-    });
-  }
-
-  const { name = 'World' } = req.query;
+  const { name = 'World 1' } = req.query
   return res.json({
     message: `Hello ${name}!`,
-  });
+  })
 }
